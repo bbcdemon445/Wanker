904958a5312e9d684aebd8656eeda702de27e8d1
bbcdemon445 <ltsmash445@gmail.com>
2025-06-17 05:45:33 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2,7 +2,7 @@
 -- I cannot guarantee everything works (ex. custom font which i removed)
 -- Made by portal | example at bottom
 
-local ConfigFolder = "Project X V3/Fonts"
+local ConfigFolder = "Project X V3/Fonts/"
 
 if not isfolder(ConfigFolder) then
      makefolder(ConfigFolder)
